16c1518e05d06cabb59f3cbfb2908121a57f358d
viktorM-web <81855790+viktorM-web@users.noreply.github.com>
2023-02-20 16:15:18 +0200

    done homework with corrections
    * done homework
    
    * corrected homework

---
+++ src/test/java/com/dmdev/dao/SubscriptionDaoIT.java
@@ -0,0 +1,148 @@
+package com.dmdev.dao;
+
+import com.dmdev.entity.Subscription;
+import com.dmdev.integration.IntegrationTestBase;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+
+import static com.dmdev.entity.Provider.APPLE;
+import static com.dmdev.entity.Provider.GOOGLE;
+import static com.dmdev.entity.Status.ACTIVE;
+import static com.dmdev.entity.Status.EXPIRED;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SubscriptionDaoIT extends IntegrationTestBase {
+
+    private final SubscriptionDao dao = SubscriptionDao.getInstance();
+
+    @Test
+    void findAll() {
+        var subscription1 = dao.insert(getSubscription(1, "Music"));
+        var subscription2 = dao.insert(getSubscription(2, "Map"));
+        var subscription3 = dao.insert(getSubscription(3, "Move"));
+
+        List<Subscription> subscriptionList = dao.findAll();
+
+        assertThat(subscriptionList).hasSize(3);
+        List<Integer> subscriptionId = subscriptionList.stream()
+                .map(Subscription::getId)
+                .toList();
+        assertThat(subscriptionId).contains(subscription1.getId(), subscription2.getId(), subscription3.getId());
+
+    }
+
+    @Test
+    void findByIdIfEntityExist() {
+        var subscription = dao.insert(getSubscription(1, "Music"));
+
+        Optional<Subscription> actualResult = dao.findById(subscription.getId());
+
+        assertThat(actualResult).isPresent();
+        assertThat(actualResult.get()).isEqualTo(subscription);
+    }
+
+    @Test
+    void findByIdIfEntityNotExist() {
+        var subscription = dao.insert(getSubscription(1, "Music"));
+
+        Optional<Subscription> actualResult = dao.findById(123456789);
+
+        assertThat(actualResult).isEmpty();
+    }
+
+    @Test
+    void deleteExistingEntity() {
+        var subscription = dao.insert(getSubscription(1, "Music"));
+
+        Boolean actualResult = dao.delete(subscription.getId());
+
+        assertTrue(actualResult);
+    }
+
+    @Test
+    void deleteNotExistingEntity() {
+        var subscription = dao.insert(getSubscription(1, "Music"));
+
+        boolean actualResult = dao.delete(123456789);
+
+        assertFalse(actualResult);
+    }
+
+    @Test
+    void update() {
+        Subscription subscription = getSubscription(1, "Music");
+        dao.insert(subscription);
+        subscription.setStatus(EXPIRED);
+        subscription.setProvider(GOOGLE);
+
+        dao.update(subscription);
+
+        Subscription updatedSubscription = dao.findById(subscription.getId()).get();
+        assertThat(updatedSubscription).isEqualTo(subscription);
+    }
+
+    @Test
+    void insert() {
+        Subscription subscription = getSubscription(1, "Music");
+
+        Subscription insert = dao.insert(subscription);
+
+        assertNotNull(insert.getId());
+    }
+
+    @Test
+    void findByUserId() {
+        var subscription1 = dao.insert(getSubscription(1, "Music"));
+        var subscription2 = dao.insert(getSubscription(1, "Map"));
+        var subscription3 = dao.insert(getSubscription(2, "Move"));
+
+        List<Subscription> subscriptions = dao.findByUserId(subscription1.getUserId());
+
+        assertThat(subscriptions).hasSize(2);
+        List<Integer> subscriptionId = subscriptions.stream()
+                .map(Subscription::getUserId)
+                .toList();
+        assertThat(subscriptionId).containsOnly(subscription1.getUserId());
+
+    }
+
+    @Test
+    void upsertIfEntityNotExist() {
+        Subscription subscription = getSubscription(1, "Music");
+
+        Subscription upsert = dao.upsert(subscription);
+
+        assertNotNull(upsert.getId());
+    }
+
+    @Test
+    void upsertIfEntityExist() {
+        Subscription subscription = getSubscription(1, "Music");
+        dao.insert(subscription);
+        subscription.setStatus(EXPIRED);
+        subscription.setProvider(GOOGLE);
+
+        dao.upsert(subscription);
+
+        Subscription updatedSubscription = dao.findById(subscription.getId()).get();
+        assertThat(updatedSubscription).isEqualTo(subscription);
+    }
+
+    private static Subscription getSubscription(Integer userId, String name) {
+        return Subscription.builder()
+                .userId(userId)
+                .name(name)
+                .provider(APPLE)
+                .expirationDate(Instant.now().plus(Duration.ofDays(10)).truncatedTo(ChronoUnit.SECONDS))
+                .status(ACTIVE)
+                .build();
+    }
+}
---
+++ src/test/java/com/dmdev/service/SubscriptionServiceTest.java
@@ -0,0 +1,142 @@
+package com.dmdev.service;
+
+import com.dmdev.dao.SubscriptionDao;
+import com.dmdev.dto.CreateSubscriptionDto;
+import com.dmdev.entity.Subscription;
+import com.dmdev.exception.SubscriptionException;
+import com.dmdev.exception.ValidationException;
+import com.dmdev.mapper.CreateSubscriptionMapper;
+import com.dmdev.validator.CreateSubscriptionValidator;
+import com.dmdev.validator.Error;
+import com.dmdev.validator.ValidationResult;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+import static com.dmdev.entity.Provider.APPLE;
+import static com.dmdev.entity.Status.ACTIVE;
+import static com.dmdev.entity.Status.EXPIRED;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class SubscriptionServiceTest {
+
+    @Mock
+    private SubscriptionDao subscriptionDao;
+    @Mock
+    private CreateSubscriptionMapper createSubscriptionMapper;
+    @Mock
+    private CreateSubscriptionValidator createSubscriptionValidator;
+    private Clock clock;
+    @InjectMocks
+    private SubscriptionService subscriptionService;
+
+    @Test
+    void expire() {
+        Subscription subscription = getSubscription();
+
+        doReturn(Optional.of(subscription)).when(subscriptionDao).findById(subscription.getId());
+
+        var exception = assertThrows(SubscriptionException.class,
+                () -> subscriptionService.expire(subscription.getId()));
+        assertThat(exception.getMessage())
+                .isEqualTo(String.format("Subscription %d has already expired", subscription.getId()));
+    }
+
+    @Test
+    void cancelIfSubscriptionNotActive() {
+        Subscription subscription = getSubscription();
+
+        doReturn(Optional.of(subscription)).when(subscriptionDao).findById(subscription.getId());
+
+        var exception = assertThrows(SubscriptionException.class,
+                () -> subscriptionService.cancel(subscription.getId()));
+        assertThat(exception.getMessage())
+                .isEqualTo(String.format("Only active subscription %d can be canceled", subscription.getId()));
+    }
+
+    @Test
+    void cancelIfSubscriptionActive() {
+        Subscription subscription = getSubscription();
+        subscription.setStatus(ACTIVE);
+
+        doReturn(Optional.of(subscription)).when(subscriptionDao).findById(subscription.getId());
+
+        subscriptionService.cancel(subscription.getId());
+
+        assertThat(subscription.getStatus()).isNotEqualTo(ACTIVE);
+    }
+
+    @Test
+    void upsertIfInvalidDto() {
+        Instant instant = Instant.now().plus(Duration.ofDays(10).truncatedTo(SECONDS));
+        CreateSubscriptionDto dto = getSubscriptionDto(instant);
+
+        var validationResult = new ValidationResult();
+        validationResult.add(Error.of(101, "name is invalid"));
+        doReturn(validationResult).when(createSubscriptionValidator).validate(dto);
+
+        assertThrows(ValidationException.class, () -> subscriptionService.upsert(dto));
+    }
+
+    @Test
+    void upsertIfValidDtoAndNotExistInDataBase() {
+        Instant instant = Instant.now().plus(Duration.ofDays(10).truncatedTo(SECONDS));
+        CreateSubscriptionDto dto = getSubscriptionDto(instant);
+        Subscription subscription = getSubscription().setExpirationDate(instant);
+
+        doReturn(new ValidationResult()).when(createSubscriptionValidator).validate(dto);
+        doReturn(List.of()).when(subscriptionDao).findByUserId(dto.getUserId());
+        doReturn(subscription).when(createSubscriptionMapper).map(dto);
+        doReturn(subscription).when(subscriptionDao).upsert(subscription);
+
+        Subscription actualSubscription = subscriptionService.upsert(dto);
+
+        assertThat(actualSubscription).isEqualTo(subscription);
+    }
+
+    @Test
+    void upsertIfValidDtoAndExistInDataBase() {
+        Instant instant = Instant.now().plus(Duration.ofDays(10).truncatedTo(SECONDS));
+        CreateSubscriptionDto dto = getSubscriptionDto(instant);
+        Subscription subscription = getSubscription().setExpirationDate(instant);
+
+        doReturn(new ValidationResult()).when(createSubscriptionValidator).validate(dto);
+        doReturn(List.of(subscription)).when(subscriptionDao).findByUserId(dto.getUserId());
+        doReturn(subscription).when(subscriptionDao).upsert(subscription);
+
+        Subscription actualSubscription = subscriptionService.upsert(dto);
+
+        assertThat(actualSubscription).isEqualTo(subscription.setStatus(ACTIVE));
+    }
+
+    private static Subscription getSubscription() {
+        return Subscription.builder()
+                .userId(11)
+                .name("Music")
+                .provider(APPLE)
+                .expirationDate(Instant.now().plus(Duration.ofDays(10).truncatedTo(SECONDS)))
+                .status(EXPIRED)
+                .build();
+    }
+
+    private static CreateSubscriptionDto getSubscriptionDto(Instant instant) {
+        return CreateSubscriptionDto.builder()
+                .userId(11)
+                .name("Music")
+                .provider(APPLE.name())
+                .expirationDate(instant)
+                .build();
+    }
+}
